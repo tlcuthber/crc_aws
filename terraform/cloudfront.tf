95713146d1836f4499d1f4a9c5cb1048474d0530
Yoko <73258869+y0k0r@users.noreply.github.com>
2024-11-12 20:39:42 -0500

    feat: add CloudFront distribution for new domain
    - Set up CloudFront distribution for toyokorivera.com
    - Add S3 origin with custom HTTP and HTTPS ports
    - Enforce HTTPS with viewer protocol policy
    - Configure default cache behavior with optimized caching
    - Apply ACM certificate for SSL support

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -39,3 +39,46 @@ resource "aws_cloudfront_distribution" "cf-distribution" {
     ssl_support_method       = "sni-only"
   }
 }
+
+# ToyokoRivera.com
+data "aws_cloudfront_cache_policy" "managed_caching_optimized_tr" {
+  name = "Managed-CachingOptimized"
+}
+
+resource "aws_cloudfront_distribution" "cf_distribution" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.tr_com.website_endpoint
+    origin_id   = "toyokorivera.com"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  aliases      = ["toyokorivera.com", "www.toyokorivera.com"]
+  enabled      = true
+  http_version = "http2and3"
+  price_class  = "PriceClass_100"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "toyokorivera.com"
+    viewer_protocol_policy = "redirect-to-https"
+    cache_policy_id        = data.aws_cloudfront_cache_policy.managed_caching_optimized_tr.id
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation_tr_com.certificate_arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+}
