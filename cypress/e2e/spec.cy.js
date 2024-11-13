0947c7df4291ac8ae55f05753f4e74b168db7ab9
Yoko <73258869+y0k0r@users.noreply.github.com>
2024-11-12 21:03:54 -0500

    test: Add cypress checks for toyokorivera.com

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,17 @@
 describe('200 Status Code Check', () => {
   it('Checks status code is equal to 200 for toyokocuthbert.com', () => {
-    cy.request('GET', 'https://toyokocuthbert.com').its('status').should('eq',200)
+    cy.request('GET', 'https://toyokocuthbert.com').its('status').should('eq', 200)
   })
 
   it('Checks status code is equal to 200 for www.toyokocuthbert.com', () => {
-    cy.request('GET', 'https://www.toyokocuthbert.com').its('status').should('eq',200)
+    cy.request('GET', 'https://www.toyokocuthbert.com').its('status').should('eq', 200)
   })
 
+  it('Checks status code is equal to 200 for toyokorivera.com', () => {
+    cy.request('GET', 'https://toyokorivera.com').its('status').should('eq', 200)
+  })
+
+  it('Checks status code is equal to 200 for www.toyokorivera.com', () => {
+    cy.request('GET', 'https://www.toyokorivera.com').its('status').should('eq', 200)
+  })
 })
